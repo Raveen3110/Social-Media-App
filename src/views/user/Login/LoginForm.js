8c7bfdbd26557d1f19806db44cd8395bbee064b0
Raveen3110` <Raveendeep3101@gmail.com`>
2021-12-02 00:09:52 +0530

    firebase setup

--- src/views/user/Login/LoginForm.js
+++ src/views/user/Login/LoginForm.js
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "react-toastify/dist/ReactToastify.css";
 import { AvForm, AvField } from 'availity-reactstrap-validation';
 import { Button, Col, Row } from 'reactstrap';
 
 export default function LoginForm() {
+    const [formKey, setFormKey] = useState(1)
+
+   const Loginsubmit=(e,values)=>{
+    e.preventDefault();        
+   setFormKey(prev => (prev + 1)); //clear form
+    console.log("object",values)
+   }
+   const InVaildsubmit=()=>{
+    console.log("In-Vaild")
+  }
 
     return (
         <>
             <Row>    
                 <Col className="pl-5" md={10} sm={12}>
-                    <AvForm>
-                        <AvField name="name" label={<b>LOGIN ID<span className="color-red">*</span></b>} type="text" errorMessage="Invalid name" validate={{
+                    <AvForm onValidSubmit={Loginsubmit} InVaildSumit={InVaildsubmit}  key={String(formKey)}>
+                        <AvField name="name" label={<b>LOGIN ID<span className="color-red">*</span></b>} type="text" errorMessage="Please enter a name" validate={{
                             required: { value: true },
                             // pattern: { value: '^[A-Za-z0-9]+$' },
                             // minLength: { value: 6 },
                             // maxLength: { value: 16 }
                         }} />
                         <AvField name="password" label={<b>PASSWORD<span className="color-red">*</span></b>} type="password" validate={{
-                            required: { value: true, errorMessage: 'Please enter a name' },
+                            required: { value: true, errorMessage: 'Please enter a password' },
                             // pattern: { value: '^[A-Za-z0-9]+$', errorMessage: 'Your name must be composed only with letter and numbers' },
                             // minLength: { value: 6, errorMessage: 'Your name must be between 6 and 16 characters' },
                             // maxLength: { value: 16, errorMessage: 'Your name must be between 6 and 16 characters' }
                         }} />
-                        <Button className="btn mt-4" style={{width:"100%"}}>Submit</Button>
+                        <Button className="btn mt-4" type="submit" style={{width:"100%"}}>Submit</Button>
                     </AvForm>
                 </Col>
             </Row>
--- src/views/user/Login/SigninForm.js
+++ src/views/user/Login/SigninForm.js
@@ -1,48 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "react-toastify/dist/ReactToastify.css";
 import { AvForm, AvField } from 'availity-reactstrap-validation';
 import { Button, Col, Row } from 'reactstrap';
+import { auth } from '../../../config/firebase'
 
 export default function SiginForm() {
+    const [formKey, setFormKey] = useState(1)
 
-const submitHandler=(e)=>{
-  console.log("object",e)
-}
+    const SignInsubmit = (e, values) => {
+        console.log("email", values.email)
+        console.log("name", values.name)
+        console.log("number", values.number)
+        console.log("password", values.password)
+
+        e.preventDefault();
+        auth
+            .createUserWithEmailAndPassword(values.email, values.password)
+            .catch(alert("Errorrrr"));
+
+        // setFormKey(prev => (prev + 1)); // clear form
+    }
 
 
     return (
         <>
-           <Row>    
+            <Row>
                 <Col className="pl-5" md={10} sm={12}>
-                    <AvForm>
-                        <AvField name="name" label={<b>Name<span className="color-red">*</span></b>} type="text" errorMessage="Invalid name" validate={{
+                    <AvForm onValidSubmit={SignInsubmit} key={String(formKey)}>
+                        <AvField name="name" label={<b>Name<span className="color-red">*</span></b>} type="text" errorMessage="Please enter a name" validate={{
                             required: { value: true },
-                            // pattern: { value: '^[A-Za-z0-9]+$' },
-                            // minLength: { value: 6 },
-                            // maxLength: { value: 16 }
                         }} />
-                        <AvField name="number" label={<b>Mobile No.<span className="color-red">*</span></b>} type="number" errorMessage="Invalid name" validate={{
+                        <AvField name="number" label={<b>Mobile No.<span className="color-red">*</span></b>} type="number" errorMessage="Please enter a number" validate={{
                             required: { value: true },
-                            // pattern: { value: '^[A-Za-z0-9]+$' },
-                            // minLength: { value: 6 },
-                            // maxLength: { value: 10 }
                         }} />
-                        <AvField name="email" label={<b>Email ID<span className="color-red">*</span></b>} type="email" errorMessage="Invalid name" validate={{
+                        <AvField name="email" label={<b>Email ID<span className="color-red">*</span></b>} type="email" errorMessage="Please enter a email" validate={{
                             required: { value: true },
-                            // pattern: { value: '^[A-Za-z0-9]+$' },
-                            // minLength: { value: 6 },
-                            // maxLength: { value: 18 }
                         }} />
-                        
-                        <AvField name="password" label={<b>PASSWORD<span className="color-red">*</span></b>} type="password" validate={{
-                            required: { value: true, errorMessage: 'Please enter a name' },
-                            // pattern: { value: '^[A-Za-z0-9]+$', errorMessage: 'Your name must be composed only with letter and numbers' },
-                            // minLength: { value: 6, errorMessage: 'Your name must be between 6 and 16 characters' },
-                            // maxLength: { value: 16, errorMessage: 'Your name must be between 6 and 16 characters' }
+                        <AvField name="password" label={<b>Password<span className="color-red">*</span></b>} type="password" validate={{
+                            required: { value: true, errorMessage: 'Please enter a password' },
                         }} />
-                        <Button className="btn mt-4" type="submit" 
-                        style={{width:"100%",backgroundColor:"orange",border:"none"}}
-                        onClick={(e)=>submitHandler(e)}
+                        <Button className="btn mt-4" type="submit"
+                            style={{ width: "100%", backgroundColor: "orange", border: "none" }}
                         >Register</Button>
                     </AvForm>
                 </Col>
