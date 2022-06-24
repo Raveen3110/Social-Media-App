f1eb7d1296ff7d78b095c0529d4281fe9e9dc182
Raveen` <Raveendeep3101@gmail.com`>
2022-06-24 22:25:47 +0530

    firebase changes

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,7 +8,7 @@ import "firebase/compat/storage"
 const firebaseConfig = {
     apiKey: "AIzaSyBEn4E55osTXYzDOcUVZvjf5Nn2xHLoT0o",
     authDomain: "social-media-app-77ad8.firebaseapp.com",
-    projectId: "social-media-app-77ad8",
+    // projectId: "social-media-app-77ad8",
     storageBucket: "social-media-app-77ad8.appspot.com",
     messagingSenderId: "1049329716911",
     appId: "1:1049329716911:web:5de1f26a2b0ddc2fd54e5a",
